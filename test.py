8fe1b7fe42b1a22b337c7bed811aefc6016e5803
Aklahoma <messivall92@gmail.com>
2018-08-02 19:23:00 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
 btc = int(input('Введите цену биткоина: '))
 your_btc = float(input('Введите Ваше количество биткоинов: '))
+
 my_btc = btc * your_btc
 usd = 24.64
 price_sale_btc = my_btc * usd
 price_inUSD = price_sale_btc / usd
-print("Общая сумма продажи: ",round(price_sale_btc, 2), "UAH")
-print("В валюте", round(price_inUSD,2), 'USD')
+
+    print("Общая сумма продажи: ",round(price_sale_btc, 2), "UAH")
+    print("В валюте", round(price_inUSD,2), 'USD')
