a3e3a3fc4d7d5403372b65c0464d6cab5bee8587
Akla <messivall92@gmail.com>
2018-08-02 19:03:10 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 btc = int(input('Введите цену биткоина: '))
 mybtc = btc * 0.016877
-usd = 26.09
-cash = (mybtc * usd) - 6484.16
+usd = 24.64
+cash = (mybtc * usd) - 5000
 a = mybtc * usd
 b = cash/usd
 print("Общая сумма продажи: ",round(a, 2), "UAH")
